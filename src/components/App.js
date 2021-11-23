72a534cecce3ea4d471b9be7bc6edc504196a59c
Michał Płużek <michalpluzek@gmail.com>
2021-11-23 11:19:56 +0100

    Add fake delay fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,30 @@ import Word from "./Word";
 class App extends React.Component {
   state = {
     words: [],
+    isLoaded: false,
   };
 
   componentDidMount() {
+    setTimeout(this.fetchData, 3000);
+  }
+
+  fetchData = () => {
     fetch("data/words.json")
       .then((response) => response.json())
       .then((data) => {
         this.setState({
           words: data.words,
+          isLoaded: true,
         });
       });
-  }
+  };
 
   render() {
     const words = this.state.words.map((word) => (
       <Word key={word.id} nameEn={word.en} namePl={word.pl} />
     ));
 
-    return <ul>{words}</ul>;
+    return <ul>{this.state.isLoaded ? words : "Wczytuję dane"}</ul>;
   }
 }
 
