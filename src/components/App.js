7243d4a6739b56b10b3c49add6c104360a94f9b4
Michał Płużek <michalpluzek@gmail.com>
2021-11-23 10:53:14 +0100

    Init app and data

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    words: [],
+  };
+
+  render() {
+    return <div>Hello, World!</div>;
+  }
+}
+
+export default App;
